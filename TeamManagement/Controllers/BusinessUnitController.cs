5d689312e683b935ad05e6a576ae717c84fcf465
Werner Hurter <whh@exclr8.co.za>
2021-01-21 17:04:17 +0200

    Added api controllers

---
+++ TeamManagement/Controllers/BusinessUnitController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using TeamManagement.Data.Models;
+using TeamManagement.Services.Services;
+
+namespace TeamManagement.Controllers
+{
+    [ApiController]
+    public class BusinessUnitController : ControllerBase
+    {
+        private readonly ILogger<BusinessUnitController> _logger;
+        private readonly IBusinessUnitService _businessUnitService;
+
+        public BusinessUnitController(ILogger<BusinessUnitController> logger, IBusinessUnitService businessUnitService)
+        {
+            _logger = logger;
+            _businessUnitService = businessUnitService;
+        }
+
+
+        [Route("api/BusinessUnit/GetAllBusinessUnits")]
+        [HttpGet]
+        public IEnumerable<BusinessUnit> GetAllBusinessUnits()
+        {
+            return _businessUnitService.GetAllBusinessUnitsAsync().Result.ToList();
+        }
+
+
+        [Route("api/BusinessUnit/GetBusinessUnitById/{id}")]
+        [HttpGet]
+        public BusinessUnit GetBusinessUnitById(int id)
+        {
+            return _businessUnitService.GetBusinessUnitByIdAsync(id).Result;
+        }
+
+
+        [Route("api/BusinessUnit/AddBusinessUnit")]
+        [HttpPost]
+        public BusinessUnit AddBusinessUnit(BusinessUnit unit)
+        {
+            return _businessUnitService.AddBusinessUnitsAsync(unit.Name, unit.BusinessUnitTypeId, unit.BusinessUnitLocationId, unit.ParentBusinessUnitId).Result;
+        }
+    }
+}
---
+++ TeamManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using TeamManagement.Data.Models;
+using TeamManagement.Services.Services;
+
+namespace TeamManagement.Controllers
+{
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        private readonly ILogger<EmployeeController> _logger;
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(ILogger<EmployeeController> logger, IEmployeeService employeeService)
+        {
+            _logger = logger;
+            _employeeService = employeeService;
+        }
+
+        [Route("api/Employee/GetAllEmployees")]
+        [HttpGet]
+        public IEnumerable<Employee> GetGetAllEmployees()
+        {
+            return _employeeService.GetAllEmployeesAsync().Result;
+        }
+
+        [Route("api/Employee/GetEmployeeById/{id}")]
+        [HttpGet]
+        public Employee GetEmployeeById(int id)
+        {
+            return _employeeService.GetEmployeeByIdAsync(id).Result;
+        }
+    }
+}
